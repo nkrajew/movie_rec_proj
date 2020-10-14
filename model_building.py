5f87fedba1454a9c362cb0eee36a00f5fdabe7f5
nkrajew <nkrajew@umich.edu>
2020-10-14 16:02:52 -0400

    new stuff

--- model_building.py
+++ model_building.py
@@ -7,220 +7,88 @@ Created on Mon Oct 12 13:19:25 2020
 import numpy as np
 import pandas as pd
 
-# for genre recs
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
-
-# for user-based algorithm
-from sklearn.metrics.pairwise import cosine_similarity
-
 # libraries for model based approaches
-from surprise import KNNWithMeans, SVD, Dataset, Reader
+from surprise import SVD, Dataset, Reader
 from collections import defaultdict
 
+# import data
 movies = pd.read_csv('movies_cleaned.csv')
 data = pd.read_csv('combined_data.csv')
 model_data = pd.read_csv('model_data.csv')
 
-
 # create a movie title dictionary
 movie_dict = movies.drop('genres',axis=1).set_index('movieId').to_dict()
 
-###### build model-based algorithms
-
-### KNN-based
-# convert data to usable format for Surprise library
-reader = Reader(rating_scale=(1, 5))
-knn_data = Dataset.load_from_df(model_data,reader)
-
-# Create trainset 
-trainset = knn_data.build_full_trainset()
-# Use user_based true/false to switch between user-based or item-based collaborative filtering
-knn_algo = KNNWithMeans(k=5, sim_options={'name': 'pearson_baseline', 'user_based': True})
-knn_algo.fit(trainset)
-# create test set and run the trained model
-# use anti_test set to get user-item pairs that do not have ratings
-testset = trainset.build_anti_testset() 
-knn_pred = knn_algo.test(testset)
-
- # now map the predictions to each user
-knn_preds_list = defaultdict(list)
-for uid, iid, true_r, est, _ in knn_pred:
-    knn_preds_list[uid].append((iid,est))
-
-# next sort the predictions by user
-for uid, user_ratings in knn_preds_list.items():
-    user_ratings.sort(key=lambda x: x[1], reverse=True)
-    knn_preds_list[uid] = user_ratings
-
-### SVD model
-svd_model = SVD(n_factors=150, n_epochs=20, lr_all=0.008, 
-                    reg_all=0.1, random_state=24)
-svd_model.fit(trainset)
-
-# predict ratings for all user-item pairs that are not in the trainset
-svd_preds = svd_model.test(testset)
-
-svd_preds_list = defaultdict(list)
-for uid, iid, true_r, est, _ in svd_preds:
-    svd_preds_list[uid].append((iid,est))
-
-# next sort the predictions by user
-for uid, user_ratings in svd_preds_list.items():
-    user_ratings.sort(key=lambda x: x[1], reverse=True)
-    svd_preds_list[uid] = user_ratings
-
-
-###### variables needed for recommendation functions
-# needed for genre recs
-tfv = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=1)
-x = tfv.fit_transform(movies['genres'])
-cosine_sim = linear_kernel(x, x)
-
-
-###### build recommender functions
-
-def get_genre_recs(title, n_recs=5):
-    """
-    Parameters
-    ----------
-    title : string
-        A string matching the title of a movie in the 'title' column of the 
-        movies dataframe. This will be the movie to base recommendations off
-        of.
-
-    Returns
-    -------
-    rec_movies: list
-        A list of the recommended movies similar to the entered title
-        based on genre
-
-    """
-    idx = list(movies[movies['title'] == title].index)[0]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    movie_indices = [i[0] for i in sim_scores]
-    rec_movies = movies['title'].iloc[movie_indices].drop(idx).head(n_recs).to_list()
-    return rec_movies
-
-def get_title_recs(title, n_recs=5):
-    """
-    Parameters
-    ----------
-    title : string
-        title : string
-        A string matching the title of a movie in the 'title' column of the 
-        movies dataframe. This will be the movie to base recommendations off
-        of using an item-based approach.
-    n_recs : int, optional
-        Number of recommendations to return. The default is 5.
-
-    Returns
-    -------
-    rec_movies : pandas series
-        A list of the recommended movies similar to the entered title
-        based on an item-based approach.
-
-    """
-    item_pivot_table = data.pivot_table(index = ["userId"],
-                                        columns = ["title"],
-                                        values = "rating",
-                                        fill_value=0)
-    movie_watched = item_pivot_table[title]
-    sim_scores = item_pivot_table.corrwith(movie_watched)
-    sim_scores = sim_scores.sort_values(ascending=False)
-    rec_movies = sim_scores.drop(title).index[:n_recs].to_list()
-    return rec_movies
-
-def remove_rated_titles(user, user_recs, n_recs):
+# take a smaller subset of the model_data
+model_data_og = model_data.copy()
+model_data = model_data[model_data['userId'] < 1000]
     
-    return user_recs
 
-def user_based_recs(user, n_recs=5, n_users=5):
-    """
-    Parameters
-    ----------
-    user : int
-        UserId number that corresponds to a user in the data (or ratings) table.
-    n_recs : int, optional
-        Number of recommendations to return. The default is 5.
-    n_users : int, optional
-        Number of similar users to find and use in weighted rankings. The 
-        default is 5.
+###### build model-based algorithms
+def generate_train_test_data(model_data):
+    reader = Reader(rating_scale=(1,5))
+    model_df = Dataset.load_from_df(model_data, reader)
+    trainset = model_df.build_full_trainset()
+    testset = trainset.build_anti_testset()
+    return trainset, testset
 
-    Returns
-    -------
-    rec_movies : pandas series
-        A list of the recommended movies similar to the entered title
-        based on an item-based approach.
+trainset, testset = generate_train_test_data(model_data)
+### SVD model
+def run_svd_model(trainset, testset):
+    svd_model = SVD(n_factors=150, n_epochs=20, lr_all=0.008, 
+                reg_all=0.1, random_state=24)
+    svd_model.fit(trainset)
+    # predict ratings for all user-item pairs that are not in the trainset
+    svd_preds = svd_model.test(testset)
+    
+    svd_preds_list = defaultdict(list)
+    for uid, iid, true_r, est, _ in svd_preds:
+        svd_preds_list[uid].append((iid,est))
+    
+    # next sort the predictions by user
+    for uid, user_ratings in svd_preds_list.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        svd_preds_list[uid] = user_ratings
 
-    """
-    # create pivot
-    user_pivot_table = data.pivot_table(index = ["userId"],
-                                        columns = ["title"],
-                                        values = "rating",
-                                        fill_value=0)
-    # calculate the cosine similarity scores for the given user
-    # subtract one from user due to indexing mismatch
-    user_scores = pd.Series(cosine_similarity(user_pivot_table)[user-1]) 
-    # match indexing (increase by one to match pivot table)
-    user_scores.index = range(1,len(user_scores)+1)
-    # take top n similar users
-    sim_users_scores = user_scores.sort_values(ascending=False)[1:n_users+1]
-    # save top n userId's as list
-    sim_users = list(sim_users_scores.index) 
-    # create user-item matrix based on top n users
-    sim_users_df = user_pivot_table.T[sim_users].T 
-    # take the weighted sum of similar user's rankings
-    user_recs =  sim_users_scores.dot(sim_users_df) / np.array([np.abs(sim_users_scores).sum(axis=0)]) 
-    # remove the titles the user has already seen
-    liked_items = list(data[data['userId'] == user].sort_values('rating', ascending=False)['title'])
-    liked_removed = [x for x in list(user_recs.index) if x not in liked_items]
-    rec_movies = user_recs.loc[liked_removed].sort_values(ascending=False).index[:n_recs].to_list()
-    return rec_movies
+    return svd_preds_list
 
-def knn_based_recs(user, n_recs=3):
-    # retrive the top n recommendations    
-    temp = knn_preds_list.get(user)[:n_recs]
-    rec_movies = pd.Series([x[0] for x in temp]).map(movie_dict['title']).to_list()    
-    return rec_movies
+svd_preds_list = run_svd_model(trainset, testset)
+
 
 def svd_based_recs(user, n_recs):
-    # preds = defaultdict(list)
-    # for uid, iid, true_r, est, _ in svd_preds:
-    #     preds[uid].append((iid,est))
-    
-    # # next sort the predictions by user
-    # for uid, user_ratings in preds.items():
-    #     user_ratings.sort(key=lambda x: x[1], reverse=True)
-    #     preds[uid] = user_ratings
-
-    # examine top n movies for user
     temp = svd_preds_list.get(user)[:n_recs]
     rec_movies = pd.Series([x[0] for x in temp]).map(movie_dict['title']).to_list()
     
     return rec_movies
 
-def get_recs(user=None, title=None, n_recs=5, kind='genre'):
-    if kind == 'genre':
-        if title:
-            # get genre recs
-            genre_recs = get_genre_recs(title, n_recs)
-            print('='*50)
-            print('Genre Based Recommendations for {}'.format(title))
-            print('='*50)
-            for i in range(len(genre_recs)):
-                print('{}. {}'.format(i+1, genre_recs[i]))
-        else: print('Please provide a genre.')
-    elif kind == 'model':
-        if user:
-            svd_recs = svd_based_recs(user, n_recs)
-            print('='*50)
-            print('Model Based Recommendations for userId {}'.format(user))
-            print('='*50)
-            for i in range(len(svd_recs)):
-                print('{}. {}'.format(i+1, svd_recs[i]))
-        else: print('Please provide a userId.')
-    else: print('Please provide an appropriate input for kind.')
+###### user input
+# generate a sample of n movies to be ranked by the user
+def  list_to_rank(movies, n_titles=150, use_all=True):
+    if use_all:
+        movie_list = movies.drop('genres', axis=1)
+    else:
+        movie_list = data.groupby(['movieId', 'title']).agg({'rating': np.mean})
+        movie_list = movie_list[movie_list['rating'] >= 3]
+    if n_titles > len(movies):
+        print('Choose fewer samples than total movies')
+    movie_list = movie_list.sample(n_titles)
+    movie_list['rating'] = ''
+    movie_list.to_csv('rate_these.csv', index=False)
+    return print('Movie list created. Ready for ratings.')
 
-    
+def integrate_new_ratings(model_data):
+    # read in user inputs and drop nulls
+    user_ratings = pd.read_csv('rate_these.csv').dropna(axis=0)
+    userId_idx = model_data['userId'].max()+1
+    user_ratings['userId'] = userId_idx
+    user_ratings = user_ratings.drop('title', axis=1)
+    user_ratings = user_ratings[model_data.columns.to_list()]
+    model_data = model_data.append(user_ratings)
+    # update the train and test sets
+    trainset, testset = generate_train_test_data(model_data)
+    # update svd model and predictions
+    svd_preds_list = run_svd_model(trainset, testset)
+    return model_data, trainset, testset, svd_preds_list
+
+### run below if adding a new user
+model_data, trainset, testset, svd_preds_list = integrate_new_ratings(model_data)
---
+++ model_testing.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 11:48:59 2020
+
+@author: nkraj
+"""
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import mean_squared_error
+
+# for genre recs
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+# libraries for model based approaches
+from surprise import KNNWithMeans, SVD, Dataset, Reader, accuracy
+from surprise.model_selection import train_test_split, GridSearchCV
+
+movies = pd.read_csv('movies_cleaned.csv')
+data = pd.read_csv('combined_data.csv')
+model_data = pd.read_csv('model_data.csv')
+
+
+####### Optimize Parameters
+
+# create a param grid for optimizing the SVD model
+reader = Reader(rating_scale=(1, 5))
+gs_data = Dataset.load_from_df(model_data,reader)
+param_grid = {'n_factors': [50, 100, 150],
+              'n_epochs': [5, 10, 20], 
+              'lr_all': [0.002, 0.005, 0.008],
+              'reg_all': [0.02, 0.1, 0.4],
+              'random_state': [24]}
+gs = GridSearchCV(SVD, param_grid, measures=['mse'], cv=3)
+gs.fit(gs_data)
+# best RMSE score
+print(gs.best_score['mse'])
+# combination of parameters that gave the best RMSE score
+print(gs.best_params['mse'])
+
+# best params yields:
+# {'n_factors': 150, 'n_epochs': 20, 'lr_all': 0.008, 
+# 'reg_all': 0.1, 'random_state': 24}
+
+####### define functions
+# define a train_test_split function
+def my_train_test_split(data):
+    """
+    Parameters
+    ----------
+    data : dataframe
+        Should be a pandas dataframe with columns userId, movieId, and
+        rating (in that order).
+
+    Returns
+    -------
+    train : dataframe
+        A training set that has removed 10 ratings per user.
+    test : dataframe
+        A test set consisting of 10 ratings per user.
+
+    """
+    n_users = data.userId.nunique()
+    test = pd.DataFrame(columns=['userId','movieId','rating'])
+    train = data.copy()
+
+    for user in range(1,n_users+1):
+        temp = data[(data['userId'] == user) & (data['rating'] > 0)].sample(10, random_state=24)
+        test = pd.concat([test, temp])
+    train = pd.merge(train, test, left_index=True, right_index=True, how="outer", indicator=True).query('_merge=="left_only"')
+    train = train[['userId_x','movieId_x','rating_x']]
+    train = train.rename(columns={
+        'userId_x': 'userId', 
+        'movieId_x':'movieId', 
+        'rating_x':'rating'
+        })
+    return train, test
+
+# define a function for mse
+def get_mse(pred, actual):
+    pred = pred[actual.nonzero()] #ignore zeros
+    actual = actual[actual.nonzero()] #ignore zeros
+    return mean_squared_error(pred, actual)
+
+
+# create function for testing the performance of memory based recommenders
+def test_memory_based(data, kind='item'):
+    train, test = my_train_test_split(data)
+    temp = train.pivot_table(index = ["userId"],columns = ["movieId"],
+                             values = "rating",fill_value=0).to_numpy()
+    actual = test.pivot_table(index = ["userId"],columns = ["movieId"],
+                              values = "rating",fill_value=0).to_numpy()
+    if kind == 'item':
+        sim = temp.T.dot(temp) + 1e-9 # add small value to fix divide by zero
+        pred = temp.dot(sim) / np.array([np.abs(sim).sum(axis=1)])
+    elif kind == 'user':
+        sim = temp.dot(temp.T) + 1e-9 # add small value to fix divide by zero
+        pred = sim.dot(temp) / np.array([np.abs(sim).sum(axis=1)]).T
+    else:
+        print('Please choose an approriate input for kind.')
+        return
+    test_mse = get_mse(pred, actual)
+    return test_mse
+
+def test_knn_based(data):
+    """
+    Parameters
+    ----------
+    data : dataframe
+        Dataframe with columns userId, movieId, and rating in that order.
+
+    Returns
+    -------
+    test_mse : float
+        The mean squared error for the knn based algorithm.
+
+    """
+    reader = Reader(rating_scale=(1, 5))
+    knn_data = Dataset.load_from_df(data,reader)
+    trainset, testset = train_test_split(knn_data, test_size=.10, random_state=24)
+    algo = KNNWithMeans(k=5, sim_options={
+        'name': 'pearson_baseline', 
+        'user_based': True
+        })
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+    test_mse = accuracy.mse(predictions, verbose=False)
+    return test_mse
+
+def test_svd(data):
+    reader = Reader(rating_scale=(1, 5))
+    svd_data = Dataset.load_from_df(data,reader)
+    trainset, testset = train_test_split(svd_data, test_size=.10, random_state=24)
+    svd_model = SVD(n_factors=150, n_epochs=20, lr_all=0.008, 
+                    reg_all=0.1, random_state=24)
+    svd_model.fit(trainset)
+    predictions = svd_model.test(testset)
+    test_mse = accuracy.mse(predictions, verbose=False)
+    return test_mse
+
+def get_genre_recs(movies, title):
+    """
+    Parameters
+    ----------
+    movies : dataframe
+        A dataframe of movies with columns 'movieId', 'title', 'genres'.
+    title : string
+        A string matching the title of a movie in the 'title' column of the 
+        movies dataframe. This will be the movie to base recommendations off
+        of.
+
+    Returns
+    -------
+    rec_movies: list
+        A pandas series of the recommended movies similar to the entered title
+        based on genre.
+
+    """
+    # create a TF-IDF vectorization of genre
+    tfv = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=1)
+    x = tfv.fit_transform(movies['genres'])
+
+    # compute the cosine similarities of genres
+    cosine_sim = linear_kernel(x, x)
+
+    # generate recs
+    idx = list(movies[movies['title'] == title].index)[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:21]
+    movie_indices = [i[0] for i in sim_scores]
+    rec_movies = movies['title'].iloc[movie_indices]
+    return rec_movies
+
+# create df to track performance
+performance_table = pd.DataFrame(index=['MSE'], columns=['user', 'item'])
+
+
+####### testing
+
+# test memory based algorithms
+for kind in ['user', 'item']:
+    performance_table[kind] = test_memory_based(model_data, kind=kind)
+    print('{}-based MSE: {:0.4f}'.format(kind, performance_table[kind][0]))
+    
+# test knn based algorithm
+knn_mse = test_knn_based(model_data)
+performance_table['knn'] = knn_mse
+print('KNN Model MSE: {}'.format(knn_mse))
+
+# test SVD algorithm
+svd_mse = test_svd(model_data)
+performance_table['svd'] = svd_mse
+print('SVD Model MSE: {}'.format(svd_mse))
+
+performance_table
